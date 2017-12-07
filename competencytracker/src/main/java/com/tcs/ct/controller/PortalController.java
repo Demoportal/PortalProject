5d429646ebb09604be458820936441c7402f2c97
Demoportal <Demoportal@123>
2017-12-07 23:17:34 +0530

    Pathvariable changed

--- competencytracker/src/main/java/com/tcs/ct/controller/PortalController.java
+++ competencytracker/src/main/java/com/tcs/ct/controller/PortalController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PortalController {
 	
-	@RequestMapping(value="/hello")
+	@RequestMapping(value="/welcome")
 	public String hello()
 	{
 		return "Welcome To Competency Tracker";
